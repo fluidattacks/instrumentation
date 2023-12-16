f5e840f6447088747bb28a0807246bcaf2666e54
Diego Restrepo <drestrepo@fluidattacks.com>
2023-12-15 20:39:52 -0500

    feat(back): add class visitor

---
+++ agent/src/main/java/com/fluidattacks/agent/visitors/AgentClassVisitor.java
@@ -0,0 +1,76 @@
+package com.fluidattacks.agent.visitors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.commons.AdviceAdapter;
+
+public class AgentClassVisitor extends org.objectweb.asm.ClassVisitor {
+
+    protected String className;
+
+    public AgentClassVisitor(org.objectweb.asm.ClassVisitor visitor, String className) {
+        super(Opcodes.ASM7, visitor);
+
+        this.className = className;
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int methodAccess, String methodName, String methodDesc, String signature,
+            String[] exceptions) {
+        return new AgentAdviceAdapter(methodAccess, null, methodAccess, methodName, methodDesc, signature);
+    }
+
+    public static class AgentAdviceAdapter extends AdviceAdapter {
+        private final String methodName;
+        private final String className;
+        private static final List<String> TAINT_ANNOTATIONS = List
+                .of("Lorg/springframework/web/bind/annotation/RequestParam;");
+        private final ArrayList<Integer> taintedParams = new ArrayList<>();
+
+        protected AgentAdviceAdapter(int api, MethodVisitor methodVisitor, int methodAccess, String methodName,
+                String methodDesc, String className) {
+            super(api, methodVisitor, methodAccess, methodName, methodDesc);
+
+            this.methodName = methodName;
+            this.className = className;
+
+            System.out.println(String.format("Calling adapter from: %s", methodName));
+        }
+
+        @Override
+        protected void onMethodEnter() {
+            if (!taintedParams.isEmpty()) {
+                for (Integer taintedParam : taintedParams) {
+                    mv.visitTypeInsn(NEW, "com/fluidattacks/agent/taint/Node");
+                    mv.visitInsn(DUP);
+                    mv.visitVarInsn(ALOAD, taintedParam);
+                    mv.visitLdcInsn(methodName);
+                    mv.visitLdcInsn(className);
+                    mv.visitInsn(ACONST_NULL);
+                    mv.visitMethodInsn(INVOKESPECIAL, "com/fluidattacks/agent/taint/Node", "<init>",
+                            "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Lcom/fluidattacks/agent/taint/Node;)V",
+                            false);
+                    mv.visitVarInsn(ASTORE, 5);
+                    mv.visitVarInsn(ALOAD, 5);
+                    mv.visitMethodInsn(INVOKESTATIC, "com/fluidattacks/agent/taint/Graph", "add",
+                            "(Lcom/fluidattacks/agent/taint/Node;)V", false);
+                }
+            }
+        }
+
+        @Override
+        public AnnotationVisitor visitParameterAnnotation(final int parameter, final String descriptor,
+                final boolean visible) {
+            System.out.println(descriptor);
+            if (TAINT_ANNOTATIONS.contains(descriptor)) {
+                taintedParams.add(parameter + 1);
+            }
+            return super.visitParameterAnnotation(parameter, descriptor, visible);
+
+        }
+    }
+}
