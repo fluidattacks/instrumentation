b3e7e3c81170a347a75730c8e4b6ffc672eaeebd
Diego Restrepo <drestrepo@fluidattacks.com>
2023-12-27 13:31:35 -0500

    feat(back): retransform base clases
    - StringConcatFactory
    - Base64 but it not works

--- agent/src/main/java/com/fluidattacks/agent/NewAgent.java
+++ agent/src/main/java/com/fluidattacks/agent/NewAgent.java
@@ -7,8 +7,18 @@ import java.util.Base64;
 public class NewAgent {
     public static void premain(String agentArgs, Instrumentation instrumentation) throws UnmodifiableClassException {
         System.out.println("Hello World from the new agent");
+        Class[] classes = instrumentation.getAllLoadedClasses();
+
         instrumentation.addTransformer(new AgentTransform(), true);
         instrumentation.retransformClasses(Runtime.class);
         instrumentation.retransformClasses(Base64.class);
+        for (Class cls : classes) {
+            if (cls.getName().contains("StringConcatFactory")) {
+                instrumentation.retransformClasses(cls);
+            }
+            if (cls.getName().contains("Base64") || cls.getName().contains("Decoder")) {
+                instrumentation.retransformClasses(cls);
+            }
+        }
     }
 }
